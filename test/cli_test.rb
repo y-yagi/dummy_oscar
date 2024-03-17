10899570bbf32fe6a78360f2897898a78057ac22
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2024-03-18 07:35:56 +0900

    Add the test for `require` option

--- test/cli_test.rb
+++ test/cli_test.rb
@@ -8,8 +8,9 @@ class CliTest < Minitest::Test
   end
 
   def test_parse_with_argv
-    options = DummyOscar::Cli.parse(["-C", "test/fixtures/basic.yml", "-p", "3000"])
+    options = DummyOscar::Cli.parse(["-C", "test/fixtures/basic.yml", "-p", "3000", "-r", "library.rb"])
     assert_equal options[:config], "test/fixtures/basic.yml"
     assert_equal options[:port], 3000
+    assert_equal options[:library], "library.rb"
   end
 end
