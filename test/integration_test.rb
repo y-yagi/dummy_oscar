eb37a5a9aa4682cec3521fe650aa9d49196b7739
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2024-03-17 12:46:59 +0900

    Split assertions to methods

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -4,7 +4,10 @@ require "test_helper"
 require "net/http"
 
 class IntegrationTest < Minitest::Test
-  def setup
+  include Minitest::Hooks
+
+
+  def before_all
     @pid = Process.spawn("./exe/dummy_oscar", "-C", "test/fixtures/basic.yaml")
     @host = "localhost"
     @port = "8282"
@@ -12,29 +15,37 @@ class IntegrationTest < Minitest::Test
     super
   end
 
-  def teardown
+  def after_all
     Process.kill(:KILL, @pid)
     sleep 1
     super
   end
 
-  def test_basic
+  def test_get
     res = Net::HTTP.start(@host, @port) {|http| http.get("/") }
     assert_instance_of Net::HTTPOK, res
     assert_equal "hello,world", res.body
+  end
 
+  def test_not_found
     res = Net::HTTP.start(@host, @port) {|http| http.get("/not_found") }
     assert_instance_of Net::HTTPNotFound, res
     assert_equal "Not found", res.body
+  end
 
+  def test_post
     headers = {'Content-type' => 'application/json; charset=UTF-8'}
     res = Net::HTTP.start(@host, @port) {|http| http.post("/users", "", headers) }
     assert_instance_of Net::HTTPNoContent, res
+  end
 
+  def test_get_data_from_file
     res = Net::HTTP.start(@host, @port) {|http| http.get("/users") }
     assert_instance_of Net::HTTPOK, res
     assert_equal JSON.parse(File.read("test/fixtures/users.json")), JSON.parse(res.body)
+  end
 
+  def test_regex_path
     res = Net::HTTP.start(@host, @port) {|http| http.get("/users/1/books") }
     assert_instance_of Net::HTTPOK, res
     assert_equal "messages", res.body
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,4 +4,5 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "dummy_oscar"
 require "debug"
 
+require "minitest/hooks/test"
 require "minitest/autorun"
